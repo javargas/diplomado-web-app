7d914a3228ff0d4857441113014399aaf6fec4b8
John Vargas <jvargas@appamia.com>
2021-09-03 17:18:41 -0500

    Adding id_product

--- sales_ms/sales_ms/models/sales_model.py
+++ sales_ms/sales_ms/models/sales_model.py
@@ -4,7 +4,7 @@ class Sales(models.Model):
 
     id = models.AutoField(primary_key = True)
     name = models.CharField(max_length = 20)
-    description = models.TextField(max_length = 140)
+    id_product = models.IntegerField(max_length = 140)
 
     class Meta:
         app_label = 'sales_ms'
